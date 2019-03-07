8ba13d882055638b2423ee1716606d8dc15e4627
H M Ata-E-Rabbi <mehrab2603@yahoo.com>
2019-03-06 20:55:07 -0700

    updated code

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import rospy
 import cv2
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import cv_bridge
 from geometry_msgs.msg import Twist, Pose
 from smach import State, StateMachine
@@ -28,88 +29,113 @@ TAG_POSE = None
 CURRENT_POSE = None
 client = None
 TAGS_IN_TOTAL = 3
+CURRENT_STATE = None
 
 
 class Turn(State):
     def __init__(self):
-        State.__init__(self, outcomes=["find_far"])
+        State.__init__(self, outcomes=["find_far"], output_keys=["current_marker"])
         self.rate = rospy.Rate(10)
         self.cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
-        self.image_sub = rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.marker_callback)
+        self.marker_sub = rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.marker_callback)
         self.marker_detected = False
         self.rate = rospy.Rate(30)
 
     def execute(self, userdata):
+        global CURRENT_STATE
+        CURRENT_STATE = "turn"
+
+        self.marker_detected = False
         while not self.marker_detected:
             msg = Twist()
             msg.linear.x = 0.0
-            msg.angular.z = 0.4
+            msg.angular.z = -0.4
             self.cmd_pub.publish(msg)
             self.rate.sleep()
         
-        print("hello")
+        userdata.current_marker = TAGS_FOUND[-1]
         self.marker_detected = False
         self.cmd_pub.publish(Twist())
 
         return "find_far"
 
-
     def marker_callback(self, msg):
         global TAG_POSE, TAGS_FOUND
+        global CURRENT_STATE
 
-        if len(msg.markers) > 0:
+        if CURRENT_STATE == "turn" and len(msg.markers) > 0:
             msg = msg.markers[0]
-            print(TAGS_FOUND)
         
             if msg.id not in TAGS_FOUND:
                 TAGS_FOUND.append(msg.id)
                 TAG_POSE = msg.pose.pose
-                print(self.marker_detected)
                 self.marker_detected = True
 
 
 class MoveCloser(State):
     def __init__(self):
-        State.__init__(self, outcomes=["close_enough"], output_keys=["goal"])
+        State.__init__(self, outcomes=["close_enough"], output_keys=["goal"], input_keys=["current_marker"])
         self.rate = rospy.Rate(10)
         self.vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+        self.marker_sub = rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.marker_callback)
+        self.current_marker = None
+        self.tag_pose = None
 
     def execute(self, userdata):
-        global TAG_POSE, CURRENT_POSE
+        global CURRENT_POSE
+        global CURRENT_STATE
+        CURRENT_STATE = "move_closer"
 
-        D_vector = [TAG_POSE.position.x - CURRENT_POSE.position.x,
-                    TAG_POSE.position.y - CURRENT_POSE.position.y]
-        D = np.linalg.norm(D_vector)
+        self.current_marker = userdata.current_marker
+        max_angular_speed = 0.8
+        min_angular_speed = 0.0
+        max_linear_speed = 0.8
+        min_linear_speed = 0.0
 
-        while D > 1.5:
-            D_vector = [TAG_POSE.position.x - CURRENT_POSE.position.x,
-                        TAG_POSE.position.y - CURRENT_POSE.position.y]
-            D = np.linalg.norm(D_vector)
-            Y = TAG_POSE.position.y - CURRENT_POSE.position.y
+        while self.tag_pose is None:
+            pass
 
-            t = Twist()
-            t.linear.x = 0.5
-            t.angular.z = Y
+        while True:
+            if self.tag_pose is not None and self.tag_pose.position.x < 0.5:
+                break
+            elif self.tag_pose is not None and self.tag_pose.position.x > 0.5:
+                move_cmd = Twist()
 
-            self.vel_pub.publish(t)
+                if self.tag_pose.position.x > 0.6: # goal too far
+                    move_cmd.linear.x += 0.1
+                elif self.tag_pose.position.x > 0.5: # goal too close
+                    move_cmd.linear.x -= 0.1
+                else:
+                    move_cmd.linear.x = 0
 
+                if self.tag_pose.position.y < 1e-3: # goal to the left
+                    move_cmd.angular.z -= 0.1
+                elif self.tag_pose.position.y > -1e-3: # goal to the right
+                    move_cmd.angular.z += 0.1
+                else:
+                    move_cmd.angular.z = 0
+                
+                move_cmd.linear.x = math.copysign(max(min_linear_speed, min(abs(move_cmd.linear.x), max_linear_speed)), move_cmd.linear.x)
+                move_cmd.angular.z = math.copysign(max(min_angular_speed, min(abs(move_cmd.angular.z), max_angular_speed)), move_cmd.angular.z)
+
+                move_cmd.linear.x = abs(move_cmd.linear.x)
+                
+                self.vel_pub.publish(move_cmd)
             self.rate.sleep()
-
-        s = np.sign(CURRENT_POSE.position.x - TAG_POSE.position.x)
 
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = "odom"
-        goal.target_pose.pose.position.x = TAG_POSE.position.x + s * 0.5
-        goal.target_pose.pose.position.y = TAG_POSE.position.y
-        goal.target_pose.pose.position.z = 0.0
-        goal.target_pose.pose.orientation.x = 0.0
-        goal.target_pose.pose.orientation.y = 0.0
-        goal.target_pose.pose.orientation.z = 0.0
-        goal.target_pose.pose.orientation.w = 0.0
-
+        goal.target_pose.pose.position.x = 0.2
+        
         userdata.goal = goal
 
         return "close_enough"
+
+    def marker_callback(self, msg):
+        if self.current_marker is not None:
+            for marker in msg.markers:
+                if marker.id == self.current_marker:
+                    self.tag_pose = marker.pose.pose
 
 
 class Navigate(State):
@@ -119,18 +145,43 @@ class Navigate(State):
         self.rate = rospy.Rate(10)
         self.stage = stage
 
+        self.move_base_client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+
     def execute(self, userdata):
-        global client, TAGS_FOUND, START_POSE, TAGS_IN_TOTAL
+        global client, TAGS_FOUND, START_POSE, TAGS_IN_TOTAL, CURRENT_POSE
+        global CURRENT_STATE
+        CURRENT_STATE = "navigate"
 
-        # client send move base to userdata.goal
-
-        # wait until done
-
-        # after done, check do we need to go back to
         if len(TAGS_FOUND) >= TAGS_IN_TOTAL:
             return "find_all"
+        else:
+            userdata.goal.target_pose.header.stamp = rospy.Time.now()
+            print("going to goal")
+            result = self.move_base_client.send_goal_and_wait(userdata.goal)
+            print(result)
 
-        return "done"
+            # turn_goal = MoveBaseGoal()
+            # turn_goal.target_pose.header.frame_id = "odom"
+            # turn_goal.target_pose.pose = CURRENT_POSE
+
+
+            # euler = euler_from_quaternion((CURRENT_POSE.orientation.x, CURRENT_POSE.orientation.y, CURRENT_POSE.orientation.z, CURRENT_POSE.orientation.w))
+            # quaternion = quaternion_from_euler(euler[0], euler[1], euler[2] * (-1))
+            
+
+            # turn_goal.target_pose.pose.orientation.x = quaternion[0]
+            # turn_goal.target_pose.pose.orientation.y = quaternion[1]
+            # turn_goal.target_pose.pose.orientation.z = quaternion[2]
+            # turn_goal.target_pose.pose.orientation.w = quaternion[3]
+
+            # self.move_base_client.send_goal_and_wait(turn_goal)
+            # print(result)
+
+            userdata.goal = MoveBaseGoal()
+            userdata.goal.target_pose.header.frame_id = "odom"
+            userdata.goal.target_pose.pose = START_POSE
+
+            return "done"
 
 
 def odom_callback(msg):
